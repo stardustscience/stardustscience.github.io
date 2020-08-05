456ee670c52e9d7d87bd3d3eaec7fec2854f8922
David Honegger Rogers <dhr@lanl.gov>
2020-08-05 13:25:56 -0600

    updating bib

--- assets/bib/convert.py
+++
@@ -1,3 +0,0 @@
-from pybtex.database.format import format_database
-
-format_database('stardust_results.bib', '../../stardust_publications.html', 'bibtex', 'html')
---
+++ pages/bib/convert.py
@@ -0,0 +1,3 @@
+from pybtex.database.format import format_database
+
+format_database('stardust_results.bib', '../publications.html', 'bibtex', 'html')
